7be6264a6dd64336e478c2142c3e1ee291bde182
Matus Kello <m.kello1992@gmail.com>
2023-06-26 20:44:05 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import questions from './data';
+import Question from './components/Question';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='all-questions'>
+      {questions.map((oneQuestion) => {
+        return <Question key={oneQuestion.id} {...oneQuestion} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
